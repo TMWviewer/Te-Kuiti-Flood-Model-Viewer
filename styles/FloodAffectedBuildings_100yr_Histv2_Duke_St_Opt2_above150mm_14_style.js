db25d5bf56b3a5d62f0aa57f49ddb94810b8f3c5
TMWviewer <rajeesh@temirowater.co.nz>
2026-09-25 15:34:05 +1200

    Add files via upload

---
+++ styles/FloodAffectedBuildings_100yr_Histv2_Duke_St_Opt2_above150mm_14_style.js
@@ -0,0 +1,33 @@
+var size = 0;
+var placement = 'point';
+
+var style_FloodAffectedBuildings_100yr_Histv2_Duke_St_Opt2_above150mm_14 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = 'left';
+    var offsetX = 8;
+    var offsetY = 3;
+    var overflow = false;
+    var repeat = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(240,0,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2.128}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth, textAlign, offsetX, offsetY, overflow, repeat)
+    })];
+
+    return style;
+};
